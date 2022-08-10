f563a9d9eb23952ead33f86e2076d52a4e47b5dd
Aleksandar Ristic <storiaca@gmail.com>
2022-08-10 08:45:10 +0200

    Expense form finished

--- expense_tracker/src/components/IncomeAndExpense.tsx
+++ expense_tracker/src/components/IncomeAndExpense.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Stack } from "react-bootstrap";
+import { useAppSelector } from "../hooks/hooks";
 
 const IncomeAndExpense = () => {
-  return <div>IncomeAndExpense</div>;
+  const income = useAppSelector((state) => state.expense.income);
+  const expense = useAppSelector((state) => state.expense.expense);
+
+  return (
+    <div className="card shadow px-1 py-3 mt-3" style={{ width: "100%" }}>
+      <Stack direction="horizontal">
+        <div className="mx-auto d-flex flex-column">
+          Expense <span className="text-danger">$ {expense}</span>
+        </div>
+        <div className="mx-auto d-flex flex-column">
+          Income <span className="text-success">$ {income}</span>
+        </div>
+      </Stack>
+    </div>
+  );
 };
 
 export default IncomeAndExpense;
