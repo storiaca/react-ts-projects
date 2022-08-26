45246032984be1a9805499ac9f7391cffe06a7fa
Aleksandar Ristic <storiaca@gmail.com>
2022-08-26 08:12:54 +0200

    Style single item

--- mern-footbal/frontend/src/features/games/GamesPage.tsx
+++ mern-footbal/frontend/src/features/games/GamesPage.tsx
@@ -1,10 +1,5 @@
 import { Container, Grid } from "@mui/material";
 import { useAppSelector } from "../../store/store";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions } from "@mui/material";
 
 const GamesPage = () => {
   const { games } = useAppSelector((state) => state.games);
@@ -14,36 +9,21 @@ const GamesPage = () => {
       <Grid container>
         {games &&
           games.map((game) => (
-            <Grid key={game._id} xs={3}>
+            <Grid
+              key={game._id}
+              xs={3}
+              sx={{
+                backgroundImage:
+                  "linear-gradient(90deg, rgb(64, 40,222), rgb(13, 200, 220))",
+                margin: 2,
+                padding: 3,
+                color: "white",
+              }}
+            >
               <h4>{game.name}</h4>
               <p>{game.address}</p>
             </Grid>
           ))}
-
-        {/* <Card sx={{ maxWidth: 345 }}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image="/static/images/cards/contemplative-reptile.jpg"
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Share
-            </Button>
-          </CardActions>
-        </Card> */}
       </Grid>
     </Container>
   );
