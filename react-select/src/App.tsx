3e5cebbd046ab0e2d66db6cc451b7aee7bab53df
Aleksandar Ristic <storiaca@gmail.com>
2022-09-24 12:52:42 +0200

    React select

---
+++ react-select/src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import Select, { SelectOption } from "./Select";
+
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fifth", value: 5 },
+];
+function App() {
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
+  return (
+    <div>
+      <Select
+        multiple
+        options={options}
+        value={value1}
+        onChange={(o) => setValue1(o)}
+      />
+      <br />
+      <Select options={options} value={value2} onChange={(o) => setValue2(o)} />
+      {/* <select name="cars" id="cars">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="opel">Opel</option>
+        <option value="audi">Audi</option>
+      </select> */}
+    </div>
+  );
+}
+
+export default App;
